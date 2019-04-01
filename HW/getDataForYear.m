345baf6e765a064e2ec48118339d83bac0384918
eocallag <elizabethmocallaghan@gmail.com>
2019-04-01 17:55:59 -0400

    getDataForYear

--- HW/getDataForYear.m
+++ HW/getDataForYear.m
@@ -3,22 +3,6 @@ function stockData = getDataForYear(stocks, dates, startYear)
 %   Detailed explanation goes here
 startIndex = find(dates==(startYear-1), 1, 'last');
 endIndex = find(dates==startYear, 1, 'last');
-
-% isLeapYear = 0;
-% if(mod(startYear,4)==0)
-%     if(mod(startYear,100)==0)
-%         if(mod(startYear,400)==0)
-%             isLeapYear = 1;
-%         else
-%             isLeapYear = 0;
-%         end
-%     else
-%         isLeapYear = 1;
-%     end
-% end
-% if(isLeapYear)
-%     endIndex = endIndex-1; 
-% end
 stockData = zeros(endIndex-startIndex,1);
 for i=1:size(stockData,1)
     stockData(i,1) = stocks(startIndex+i-1);
--- HW/load_data.m
+++ HW/load_data.m
@@ -1,24 +1,24 @@
-clear all;
-[VFINX_years, VFINX] = getDataFromFile('HW_1_data/VFINX.csv');
-disp('VFINX loaded');
-[VBMFX_years, VBMFX] = getDataFromFile('HW_1_data/VBMFX.csv');
-disp('VBMFX loaded');
-[VGSLX_years, VGSLX] = getDataFromFile('HW_1_data/VGSLX.csv');
-disp('VGSLX loaded');
-
-[VBLTX_years, VBLTX] = getDataFromFile('HW_1_data/VBLTX.csv');
-disp('VBLTX loaded');
-[VEIEX_years, VEIEX] = getDataFromFile('HW_1_data/VEIEX.csv');
-disp('VEIEX loaded');
-[VIMSX_years, VIMSX] = getDataFromFile('HW_1_data/VIMSX.csv');
-disp('VIMSX loaded');
-
-[MSFT_years, MSFT] = getDataFromFile('HW_1_data/MSFT.csv');
-disp('Microsoft loaded');
-[WFC_years, WFC] = getDataFromFile('HW_1_data/WFC.csv');
-disp('Wells Fargo loaded');
-[UPS_years, UPS] = getDataFromFile('HW_1_data/UPS.csv');
-disp('UPS loaded');
+% clear all;
+% [VFINX_years, VFINX] = getDataFromFile('HW_1_data/VFINX.csv');
+% disp('VFINX loaded');
+% [VBMFX_years, VBMFX] = getDataFromFile('HW_1_data/VBMFX.csv');
+% disp('VBMFX loaded');
+% [VGSLX_years, VGSLX] = getDataFromFile('HW_1_data/VGSLX.csv');
+% disp('VGSLX loaded');
+% 
+% [VBLTX_years, VBLTX] = getDataFromFile('HW_1_data/VBLTX.csv');
+% disp('VBLTX loaded');
+% [VEIEX_years, VEIEX] = getDataFromFile('HW_1_data/VEIEX.csv');
+% disp('VEIEX loaded');
+% [VIMSX_years, VIMSX] = getDataFromFile('HW_1_data/VIMSX.csv');
+% disp('VIMSX loaded');
+% 
+% [MSFT_years, MSFT] = getDataFromFile('HW_1_data/MSFT.csv');
+% disp('Microsoft loaded');
+% [WFC_years, WFC] = getDataFromFile('HW_1_data/WFC.csv');
+% disp('Wells Fargo loaded');
+% [UPS_years, UPS] = getDataFromFile('HW_1_data/UPS.csv');
+% disp('UPS loaded');
 
 numYears = 13;
 tradingDays = 252;
@@ -28,6 +28,8 @@ data_15 = zeros(tradingDays,9);
 data_16 = zeros(tradingDays,9);
 data_17 = zeros(tradingDays-1,9);
 data_18 = zeros(tradingDays-1,9);
+
+getDataForYear(MSFT, MSFT_years,2018)
 
 
 data_14(:,1) = getDataForYear(VFINX, VFINX_years, 2014);
@@ -47,9 +49,6 @@ data_15(:,5) = getDataForYear(VEIEX, VEIEX_years, 2015);
 data_15(:,6) = getDataForYear(MSFT, MSFT_years, 2015);
 data_15(:,8) = getDataForYear(WFC, WFC_years, 2015);
 data_15(:,9) = getDataForYear(UPS, UPS_years, 2015);
-
-
-
 
 data_16(:,1) = getDataForYear(VFINX, VFINX_years, 2016);
 data_16(:,2) = getDataForYear(VBMFX, VBMFX_years, 2016);
