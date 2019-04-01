6d6a80fe2f88f99228dd2befe8c7d5bfe5979e9d
eocallag <elizabethmocallaghan@gmail.com>
2019-04-01 19:06:35 -0400

    correlation by asset in cell array

--- HW/hw1.m
+++ HW/hw1.m
@@ -1,22 +1,42 @@
-load(hw1_data.mat);
+load('hw1_data.mat');
 
 
 %% Exercise One
 
-corr = cell(3);
-for i=1:3
-    currGroup = data_group(i);
-    corr_Group = cell(3)
-    for j = 1:3
-        q1 = calcQuarter(cell2mat(currGroup(j)),1);
-        q2 = calcQuarter(cell2mat(currGroup(j)),1);
-        q3 = calcQuarter(cell2mat(currGroup(j)),1);
-        q4 = calcQuarter(cell2mat(currGroup(j)),1);
+corr = cell(1,5);
+for i=1:5
+    curr_data = cell2mat(data_years(i));
+    corr_Group = cell(1,9);
+    for j = 1:9
+        q1 = calcQuarter(curr_data(:,j),1);
+        q2 = calcQuarter(curr_data(:,j),2);
+        q3 = calcQuarter(curr_data(:,j),3);
+        q4 = calcQuarter(curr_data(:,j),4);
         correlations = [autoCorr(q1), autoCorr(q2), autoCorr(q3), autoCorr(q4)];
-        corr_Group(j)= correlations;
+        corr_Group{j}= correlations;
     end
-    corr(i) = corr_Group;
+    corr{i} = corr_Group;
 end
+
+num_years = 5;
+corr2 = cell(1,3);
+for i=1:3
+    corr_asset = cell(1,3);
+    for j = 1:3
+        correlations = zeros(num_years,4);
+        for k=1:num_years
+            curr_data = data_group{1,i}{1,k};
+            q1 = calcQuarter(curr_data(:,j),1);
+            q2 = calcQuarter(curr_data(:,j),2);
+            q3 = calcQuarter(curr_data(:,j),3);
+            q4 = calcQuarter(curr_data(:,j),4);
+            correlations(k,:) = [autoCorr(q1), autoCorr(q2), autoCorr(q3), autoCorr(q4)];
+        end
+        corr_asset{j}= correlations;
+    end
+    corr2{i} = corr_asset;
+end
+
 
 
 
