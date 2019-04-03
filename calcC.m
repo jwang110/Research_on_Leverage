c2bcaa9eb61f228f3b2886844d20ad378b917e9b
eocallag <elizabethmocallaghan@gmail.com>
2019-04-03 17:24:53 -0400

    example in vim

--- calcC.m
+++ calcC.m
@@ -1,7 +1,4 @@
 function C = calcC(stock)
-%example on git
-% another example ecause im learning
-%example on how to push in git
 C = zeros(size(stock,2),size(stock,2));
 for i=1:size(stock,2)
     for j=1:size(stock,2)
