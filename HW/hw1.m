6a9c79a5aa949b4db086f868adbc203de6b5ec53
lnorthcu <lnorthcu@umd.edu>
2019-04-03 23:32:12 -0400

    changed titles for graphs in ex 1, adding comments in ex 3

--- HW/hw1.m
+++ HW/hw1.m
@@ -7,6 +7,7 @@ num_years = 5;
 years = [2014, 2014.25, 2014.5, 2014.75, 2015, 2015.25, 2015.5, 2015.75, 2016, 2016.25, 2016.5, 2016.75,2017, 2017.25, 2017.5, 2017.75,2018, 2018.25, 2018.5, 2018.75];
 assets={'VFINX','VBMFX','VGSLX','VBLTX','VEIEX','VIMSX','Microsoft','Wells Fargo','UPS'};
 average_cor=[];
+h=0;
 for i=1:3
     for j = 1:3
         correlations = [];
@@ -18,9 +19,10 @@ for i=1:3
             q4 = calcQuarter(curr_data(:,j),4);
             correlations = horzcat(correlations,[autoCorr(q1), autoCorr(q2), autoCorr(q3), autoCorr(q4)]);
         end
+        h=h+1;
         figure;
         plot(years, correlations);
-        title(assets(i),'FontSize',font + 4);
+        title(assets(h),'FontSize',font + 4);
             xlabel('Year','FontSize',font)
             ylabel('Correlations', 'FontSize',font)
         average_cor=[average_cor;mean(correlations)];
@@ -95,11 +97,23 @@ T=table(assets,average_ome)
 T=table(assets,average_cor)
 % From the graphs in the two exercises above and the average identically distributed omega value,
 % we can see that almost all the assets do not have a significant
-% correlation. 
+% correlation, and can be described by an IID model. Some years for
+% specific assets are better described by an IID model than others, for
+% example
+
+% As seen in the tables, all of the assets except VEIEX have negative average correlation values,
+% and Wells Fargo and UPS have the closest values to 0 (although they are
+% all to the power of 10^2 or 10^3). This implies that these assets do not
+% have a significant correlation.
+
+
+
 % *VBMFX* has the highest average identically distributed value, which was
 % only approximately .27
 % *VBLTX* has the lowest average identically distributed value, which was
 % approximately .18
+
+
 % The correlation calculated in both way are around 0.2 which is low. The
 % correlation means the asset price for each year are independent. There
 % are some assets th lower correlation than others. For example VBLTX,
