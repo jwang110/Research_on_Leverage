bbce71a5e7f4b893eb282519694529aa6f5547df
eocallag <elizabethmocallaghan@gmail.com>
2019-04-24 21:19:45 -0400

    prox changes

--- Project/proximity.m
+++ Project/proximity.m
@@ -1,33 +1,32 @@
-function proximity(filename, dates, m, var)
+function proximity(dates, m, var)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-load(filename);
-numyear=size(data,2);
-per=size(data{1,1},2);
-s=size(data{1,1}{1,1},2);
+numyear=size(var,2);
+per = size(var{1,1},2);
+s = size(var{1,1}{1,1},2);
 font=20;
 
-prox = zeros(s,numyear*per);
+prox = zeros(numyear*per, s);
 index= 1;
 
 for i=1:numyear
     for j=1:per
-        prox(:, index) = calcProx(var{1,i}{1,j},m{1,i}{1,j}',s);
+        prox(index, :) = calcProx(var{1,i}{1,j},m{1,i}{1,j}',s);
+        index = index+1;
     end
 end
 
-prox_diff = zeros(s, numyear*per-1);
-for i=2:index
-    prox_diff(i,:) = prox(i,:)-prox(i-1,:);
+prox_diff = zeros(numyear*per-1,s);
+for i=2:numyear*per
+    prox_diff(i-1,:) = prox(i,:)-prox(i-1,:);
 end
-
-figure;
-hold all;
-datetick('x','yyyy');
 
 for i=1:s
-   plot(dates(1,2:end, prox_diff(:,1)));    
+    figure;
+    hold on;
+    plot(dates(1,2:end), prox_diff(:,i));    
 end
+datetick('x','yyyy');
 
 end
 
