ac6601411a25ca785404bf15c43c0c470fda4778
eocallag <elizabethmocallaghan@gmail.com>
2019-04-25 15:17:38 -0400

    dim changes and prox changes

--- Project/proximity.m
+++ Project/proximity.m
@@ -6,26 +6,34 @@ per = size(var{1,1},2);
 s = size(var{1,1}{1,1},2);
 font=20;
 
-prox = zeros(numyear*per, s);
+prox = zeros(numyear*per, 1);
 index= 1;
 
 for i=1:numyear
     for j=1:per
-        prox(index, :) = calcProx(var{1,i}{1,j},m{1,i}{1,j}',s);
+        temp = calcProx(var{1,i}{1,j},m{1,i}{1,j}',s);
+        prox(index, 1) = temp(1,1);
         index = index+1;
     end
 end
 
-prox_diff = zeros(numyear*per-1,s);
+prox_diff = zeros(numyear*per-1,1);
 for i=2:numyear*per
-    prox_diff(i-1,:) = prox(i,:)-prox(i-1,:);
+    prox_diff(i-1,1) = prox(i,1)-prox(i-1,1);
 end
 
-for i=1:s
-    figure;
-    hold on;
-    plot(dates(1,2:end), prox_diff(:,i));    
-end
+% for i=1:s
+%     figure;
+%     hold on;
+%     plot(dates(1,2:end), prox_diff(:,1));    
+% end
+
+figure;
+hold on;
+yyaxis left
+plot(dates, prox(:,1));
+yyaxis right
+plot(dates(1,2:end), prox_diff(:,1));
 datetick('x','yyyy');
 
 end
