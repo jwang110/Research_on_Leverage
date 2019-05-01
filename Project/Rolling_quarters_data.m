cee62b94009e46b9c54d0f31c4b58473c9161581
jwang110 <jwang110@terpmail.umd.edu>
2019-05-01 14:25:46 -0400

    adding the rolling quarter data

---
+++ Project/Rolling_quarters_data.m
@@ -0,0 +1,17 @@
+load('data_quart_2002.mat');
+
+numyear=size(data,2);
+per=size(data{1,1},2);
+s=size(data{1,1}{1,1},2);
+n=1;
+for i=1:numyear
+    for j=1:per
+        all_quarter_data{1,n}=data{1,numyear}{1,per}
+        n=n+1;
+    end
+end
+
+for k=1:size(all_quarter_data,2)-3
+    rolling_quarter{1,k}=[all_quarter_data{1,k};all_quarter_data{1,k+1};...
+        all_quarter_data{1,k+3};all_quarter_data{1,k+3}];
+end
